c8d04a5a4a480269eddde7e4f48fc6560569a3c7
jcy0228 <ryon.jeong@gmail.com>
2019-04-10 16:56:15 +0900

    로컬 웹서버와 통신 성공

--- bankMonitor/popup.js
+++ bankMonitor/popup.js
@@ -6,8 +6,17 @@ captureBtn.onclick = function(element) {
         // console.log(tabs[0].id);
 
         chrome.pageCapture.saveAsMHTML({tabId: tabs[0].id}, function(mhtml) {
-            console.log(mhtml);
-            // saveAs(mhtml, tab.title+'.mhtml');
+            // console.log(mhtml);
+
+            var xhr = new XMLHttpRequest(), formData = new FormData();  
+            formData.append("mhtml", mhtml);
+            // formData.append("surveyID", localStorage["ID"]);
+            xhr.open("POST", "http://127.0.0.1:8080/mhtml", true);
+            // xhr.setRequestHeader('Authorization', 'Token token=<redacted>');
+            xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+            xhr.send(formData);
+
+            console.log("submitMHTML() sent mhtml to server");
         });
     });
 };
