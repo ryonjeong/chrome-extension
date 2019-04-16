2cc94cd1adceec702bbc83388ed3d4c7e6e99d66
jcy0228 <ryon.jeong@gmail.com>
2019-04-16 15:34:19 +0900

    mhtml 고서버로 업로드 성공

--- bankMonitor/popup.js
+++ bankMonitor/popup.js
@@ -8,15 +8,46 @@ captureBtn.onclick = function(element) {
         chrome.pageCapture.saveAsMHTML({tabId: tabs[0].id}, function(mhtml) {
             // console.log(mhtml);
 
-            var xhr = new XMLHttpRequest(), formData = new FormData();  
-            formData.append("mhtml", mhtml);
-            // formData.append("surveyID", localStorage["ID"]);
-            xhr.open("POST", "http://127.0.0.1:8080/mhtml", true);
-            // xhr.setRequestHeader('Authorization', 'Token token=<redacted>');
-            xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-            xhr.send(formData);
+            var XHR = new XMLHttpRequest();
+            var FD = new FormData();
+            var FREADER = new FileReader()
 
-            console.log("submitMHTML() sent mhtml to server");
+            FD.append("Name", "MyName");
+            FD.append("blob", mhtml, "bank.mhtml");
+            // console.log(FREADER.readAsBinaryString(mhtml))
+            // FD.append("file",FREADER.readAsBinaryString(mhtml))
+
+            XHR.addEventListener('load', function(event){
+                console.log("Data sent and response loaded.");
+            });
+
+            XHR.addEventListener('error', function(event){
+                console.log("OOOPS! something went wrong.");
+            });
+
+            XHR.open('POST', 'http://127.0.0.1:8888/mhtml', true)
+
+            XHR.setRequestHeader('Authorization', 'Token token=<redacted>');
+
+            XHR.send(FD);
+
+            // var xhr = new XMLHttpRequest(), formData = new FormData();  
+            // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            // formData.append("test", "mystring")
+            // formData.append("mhtml", mhtml)
+            // formData.append("surveyID", localStorage["ID"]);
+
+            // xhr.open("POST", "http://127.0.0.1:8888/mhtml", true);
+            // xhr.setRequestHeader('Authorization', 'Token token=<redacted>');
+            // xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+
+            // xhr.send(formData);
+            // xhr.send();
+            // xhr.onload = function() {
+            //     alert(xhr.responseText)
+            // }
+
+            // console.log("submitMHTML() sent mhtml to server");
         });
     });
 };
